2a875b06945ebe264fd7a07809925f6c5d0e3dc1
daniel0128 <survey600@qq.com>
2020-04-30 01:40:43 +0800

    coding and pics

--- week03/convertNumberToString.js
+++ week03/convertNumberToString.js
@@ -1,16 +1,38 @@
-function convertNumberToString(number, scale = 10) {
-    if (scale<2 || scale>36) return;
-    number = Number(number);
-    let symbolArr = '0123456789abcdefghijklmnopqrstuvwxyz'.split('');
-    if (number % 1 === 0) { // integer
-
-    } else { // float
-        // decimal
-
-        // eE
-
+function convertNumberToString(number, radix = 10) {
+    if (radix%1!==0 || radix<2 || radix>36) return;
+    if (typeof number !== 'number') return;
+    let symbols = '0123456789abcdefghijklmnopqrstuvwxyz';
+    let integer = Math.floor(number);
+    let fraction = number * 10e8 % 10e8 / 10e8;
+    let string = integer === 0 ? '0' : '';
+    while(integer>0) {
+        string = symbols[integer % radix] + string;
+        integer = Math.floor(integer/radix);
     }
-    // console.log(symbolArr);
-    return (number).toString(scale);
+    if(fraction === 0) return string;
+    string += '.';
+
+    let len = fraction.toString(radix).length - 2;
+    while(fraction !== 0 && len-- > 0) {
+
+        fraction *= 10e10 / (10e10 / radix);
+
+        if(len === 0) {
+            console.log(fraction)
+        }
+        string = string + symbols[Math.floor(fraction)];
+        fraction = fraction * 10e10 % 10e10 / 10e10
+    }
+    return string;
 }
-console.log(convertNumberToString(120))
+// const numbers = [23, 0b11001, 0o07702, 0x12fe3, 0.1, 0.5, 3e-6];
+// numbers.map(number => {
+//     for(let i = 2; i<=36; i++) {
+//         if (convertNumberToString(number, i) !== number.toString(i)) {
+//             console.log('wrong at '+ number, i);
+//             console.log(convertNumberToString(number, i))
+//             console.log(number.toString());
+//             break;
+//         }
+//     }
+// });
--- week03/convertStringToNumber.js
+++ week03/convertStringToNumber.js
@@ -1,22 +1,36 @@
-function convertStringToNumber(string, scale = 10) {
-    if (scale<2 || scale>36) return;
+function convertStringToNumber(string, radix = 10) {
+    // const numberReg = /(0|[1-9][0-9]?\.([0-9]+)?([eE][\+-]?[0-9]+)?)|(\.[0-9]+([eE][\+-]?[0-9]+)?)|[1-9][0-9]*|0[bB][01]+|0[oO][0-7]+|0[xX][0-9a-fA-F]+/;
+    // console.log(numberReg.test(string));
+    if (radix<2 || radix>36) return;
+    // if(!numberReg.test(string)) return;
+    const digitals = '0123456789abcdefghijklmnopqrstuvwxyz';
     let chars = string.split('');
     let number = 0;
     let i = 0;
     while (i < chars.length && chars[i] != '.') {
-        number *= scale;
-        number += chars[i].codePointAt(0) - 48;
+        number *= radix;
+        let digital = digitals.indexOf(chars[i]);
+        if (digital >= radix) return number;
+        number += digital;
         i++;
     }
     let fraction = 1;
     if (chars[i] === '.') i++;
     while (i < chars.length) {
-        fraction /= scale;
-        number += (chars[i].codePointAt(0) - 48) * fraction;
+        fraction /= radix;
+        let digital = digitals.indexOf(chars[i]);
+        if (digital >= radix) return number;
+        number += digital;
         i++;
     }
     return number;
 }
 
-// console.log('100, 2: ', convertStringToNumber('100', 2));
-// console.log(convertStringToNumber('100.012345'));
+function expect(string, number) {
+    console.log(`${number} ${string === number ? '✅' : '❌'}`);
+}
+
+expect(convertStringToNumber('100', 2),4);
+expect(convertStringToNumber('ff', 16), 255);
+expect(convertStringToNumber('23'), 23);
+expect(convertStringToNumber('zz', 36), 1295);
